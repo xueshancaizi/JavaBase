38492d2d6178427125a2e6d58acadd1c487b03e6
xueshancaizi <hdu_panwei@163.com>
2016-07-18 17:21:32 +0800

    First version

---
+++ bbs/src/com/yiqiao/www/Solution.java
@@ -0,0 +1,11 @@
+package com.yiqiao.www;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+	}
+
+}
